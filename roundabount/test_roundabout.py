489fd67462551cf8c28e36af890fd2ba8cb5cc5d
masseypaul <paul.massey198@gmail.com>
2025-04-24 21:35:53 +0200

    change test for roundabout ppo and dqn

--- roundabount/test_roundabout.py
+++ roundabount/test_roundabout.py
@@ -3,6 +3,9 @@ import pickle
 import gymnasium as gym
 import highway_env
 from stable_baselines3 import DQN, PPO
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 with open("config.pkl", "rb") as f:
     config = pickle.load(f)
@@ -13,7 +16,9 @@ env = gym.make(env_name, render_mode="human")
 env.unwrapped.configure(config)
 
 model = PPO.load("ppo_roundabout/best_model.zip", env)
+model = DQN.load("dqn_roundabout/best_model.zip", env)
 
+all_rewards = []
 for i in range(10):
     total_reward = 0
     obs, _ = env.reset()
@@ -26,3 +31,7 @@ for i in range(10):
         if done:
             break
     print(total_reward)
+    all_rewards.append(total_reward)
+sns.histplot(all_rewards, bins=200, kde=True, stat='count', element='bars', linewidth=0)
+plt.savefig("test_dqn.png")
+plt.close()
